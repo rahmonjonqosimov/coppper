8f92a7e5c2d41d206ce020460cc2fe52ad553b01
Rahmonjon <qosimovrahmonjon0927@gmail.com>
2024-08-06 09:40:15 +0500

    hero section changed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -10,7 +10,7 @@ const Hero: React.FC = () => {
           <div className="w-[600px] h-[1px] bg-[#031412]"></div>
           <span className='text-[50px] block max-w-[530px] font-extrabold text-[#031412] mt-16'>Дистиллятор для эфирных масел </span>
           <div className='flex items-center justify-between max-w-[400px] my-10'> <h4 className='text-[30px] font-normal text-[#031412]'>Цена</h4> <p className='text-[30px] font-extrabold text-[#031412]'>4 906 грн</p> </div>
-          <button className='py-2 btn px-20 text-[18px] text-[#031412] font-extrabold rounded'>Купить</button>
+          <button className='py-2 btn px-20 text-[18px] text-white font-extrabold rounded'>Купить</button>
         </div>
         <img src={ image } alt="" />
       </div>
